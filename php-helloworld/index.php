d31e0fdcb532cb1b22a4f02c2e35af44b35132d5
Student User <student@workstation.lab.example.com>
2021-11-10 08:55:18 -0500

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Größe  Änderungen kommen gleich :) "
+print "Größe  Änderungen kommen gleich :) ";
 $t=time();
 echo($t . "<br>");
 echo(date("Y-m-d",$t));
